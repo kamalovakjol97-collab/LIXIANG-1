4937aab0e2c7cc0cfb348073016f31a51c01fd13
Anton <Dgylian@gmail.com>
2026-01-22 16:11:45 +0300

    63

--- prisma/migrations/20250121220000_add_status_comment/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "order_status_history" ADD COLUMN "comment" TEXT;
-
--- Update default status in orders table
-UPDATE "orders" SET "status" = 'Заявка создана' WHERE "status" = 'Заявка принята';
-
